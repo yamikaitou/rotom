9edbdfe2d799d9bd85e23621e58f1bdfda694815
Ryan LeBlanc <yamikaitou@gmail.com>
2019-06-14 13:14:41 -0400

    Add rtset and Config

--- raidtrain/raidtrain.py
+++ raidtrain/raidtrain.py
@@ -12,6 +12,10 @@ class RaidTrain(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=192153481165930496, force_registration=True)
+        
+        default_guild = {"category": 0, "copy": 0}
+        self.config.register_guild(**default_guild)
 
     @checks.mod()
     @commands.command()
@@ -26,3 +30,25 @@ class RaidTrain(commands.Cog):
             )
         else:
             await ctx.send("You must specify a number greater than 0")
+
+    @checks.admin()
+    @commands.group()
+    async def rtset(self, ctx):
+        """
+            Various settings for RaidTrain
+        """
+        pass
+    
+    @rtset.command()
+    async def category(self, ctx, category_id: discord.CategoryChannel):
+        """
+            Set the Category the RaidTrain channels are created in
+        """
+        await self.config.guild(ctx.guild).category.set(category_id)
+    
+    @rtset.command()
+    async def permission(self, ctx, channel_id: discord.TextChannel):
+        """
+            Set the channel to copy permissions from
+        """
+        await self.config.guild(ctx.guild).copy.set(channel_id)
