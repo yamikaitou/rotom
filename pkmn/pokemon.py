2430cdd7c9364e45718e8af4e2766d761497b6c2
Ryan LeBlanc <yamikaitou@gmail.com>
2019-07-24 16:07:12 -0400

    dynamic cp

--- pkmn/pokemon.py
+++ pkmn/pokemon.py
@@ -5,6 +5,7 @@ from redbot.core.utils.predicates import MessagePredicate
 import asyncio
 import aiomysql
 from typing import Union
+import math
 from .emoji import *
 
 
@@ -43,7 +44,7 @@ class Pokemon(commands.Cog):
 
         if len(r) == 1:
             embed = discord.Embed(
-                title="#001 - Bulbasaur",
+                title=f"#001 - Bulbasaur",
                 colour=discord.Colour(0xA80387),
                 description=f"{GRASS} {POISON} :sparkles:\n\n"
                 f"Weak - {FIRE} {FLYING} {ICE} {PSYCHIC}\n"
@@ -52,7 +53,22 @@ class Pokemon(commands.Cog):
                 f"Super Resists - {GRASS}\n",
             )
             embed.set_image(url="https://rotom.app/discord/pkmn/pokemon_icon_001_00.png")
-            embed.add_field(name="Perfect CP", value="Lv15 - 100\nLv20 - 200\nLv25 - 300")
+            bAtk = 118
+            bDef = 111
+            bSta = 128
+            cp15 = math.floor(
+                (bAtk * math.pow(bDef, 0.5) * math.pow(bSta, 0.5) * math.pow(0.51739395, 2)) / 10
+            )
+            cp20 = math.floor(
+                (bAtk * math.pow(bDef, 0.5) * math.pow(bSta, 0.5) * math.pow(0.5974, 2)) / 10
+            )
+            cp25 = math.floor(
+                (bAtk * math.pow(bDef, 0.5) * math.pow(bSta, 0.5) * math.pow(0.667934, 2)) / 10
+            )
+
+            embed.add_field(
+                name="Perfect CP", value=f"Lv15 - {cp15}\nLv20 - {cp20}\nLv25 - {cp25}"
+            )
             embed.add_field(
                 name="Evolutions", value="2nd: Ivysaur - 25 Candy\n3rd: Venasaur - 100 Candy"
             )
