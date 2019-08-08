552f588c05b0d6d448ef7dc9ad4108175d5a148a
Ryan LeBlanc <yamikaitou@gmail.com>
2019-08-08 16:30:53 -0400

    eod

---
+++ rotom/rotom.py
@@ -0,0 +1,22 @@
+import discord
+import random
+from redbot.core import commands, Config, checks, data_manager
+from redbot.core.utils.predicates import MessagePredicate
+import asyncio
+import aiomysql
+import aiobotocore
+from typing import Union
+import math
+import json
+from .emoji import *
+
+
+class Rotom(commands.Cog):
+    """
+    Core Rotom commands
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.config = Config.get_conf(self, identifier=1977316625, force_registration=True)
+        default_guild = {"ex": { "active": [], "channel": 0, "bucket": "" } }
