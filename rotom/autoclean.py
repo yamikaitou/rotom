31078d9eb5fba466fd371c75d5426a57bdfee303
Ryan LeBlanc <yamikaitou@gmail.com>
2019-08-15 11:48:07 -0400

    start of autoclean

---
+++ rotom/autoclean.py
@@ -0,0 +1,18 @@
+import discord
+import random
+from redbot.core import commands, Config, checks, data_manager
+from redbot.core.utils.predicates import MessagePredicate
+import asyncio
+from typing import Union
+import math
+import json
+from .emoji import *
+
+
+class AutoClean(commands.Cog):
+    """
+    Rotom's auto cleaning channels cog
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
