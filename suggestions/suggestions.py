5fa74ba378aa913a3acc90b87d6cd0cd1e4a880e
Ryan LeBlanc <yamikaitou@gmail.com>
2019-10-06 23:16:22 -0400

    force int

--- suggestions/suggestions.py
+++ suggestions/suggestions.py
@@ -136,7 +136,8 @@ class Suggestions(commands.Cog):
                         name="__________\nHow to Vote",
                         value="Simply React to this message to cast your vote\n 👍 for Yes   |   👎 for No",
                     )
-                    chan = self.bot.get_guild(id).get_channel(data["channel"])
+                    
+                    chan = self.bot.get_guild(int(id)).get_channel(int(data["channel"]))
                     msg = await chan.send(embed=embed)
                     await msg.add_reaction("👍")
                     await msg.add_reaction("👎")
